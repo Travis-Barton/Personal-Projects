4ab981dfe58d26851312bb33f9fd5a868c4b6bf9
Travis <tbarton@csumb.edu>
2019-02-03 22:48:34 -0800

    summary

---
+++ Functions for LilRhino/Untitled.R
@@ -0,0 +1,25 @@
+# Bootstrap sampling of individual words in each group of sentences seperated by class. 
+# This way we get more data for small sizes
+
+
+### 1) Turn posts into one hot encodings with data.
+
+### 2) Seperate them by class.
+
+### 3) Label each '1' in each vector with a vector and a placeholder.
+
+### 4) Make a vector of lengths of words.
+
+### 5) Sample each word randomly. The size of each word is also sampled from the 
+###    vector made in 4). 
+
+### 6) Profit! (jk but really then you have new data and can test)
+
+### 7) Functionalize it for package.
+
+
+
+
+
+
+
