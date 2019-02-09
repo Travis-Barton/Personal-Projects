9207c5bb665be2ce92f0b02c92af24f03f9dac27
Travis <tbarton@csumb.edu>
2019-02-09 00:49:50 -0800

    Update Testing word vecs.R

--- Functions for LilRhino/Testing word vecs.R
+++ Functions for LilRhino/Testing word vecs.R
@@ -1,4 +1,20 @@
+## embeddings loading
 
+library(readr)
+
+load_glove_embeddings = function(path, d){
+  #'/Users/travisbarton/Downloads/glove.6B/glove.6B.50d.txt'
+  col_names <- c("term", paste("d", 1:d, sep = ""))
+  dat <- as.data.frame(read_delim(file = path,
+                                  delim = " ",
+                                  quote = "",
+                                  col_names = col_names))
+  
+  rownames(dat) = dat$term
+  dat = dat[,-1]
+  
+  
+}
 ## 0) Pretreatment
 
 Pretreatment = function(title_vec){
@@ -14,7 +30,7 @@ Pretreatment = function(title_vec){
 
 
 StopWordMaker = function(titles, cutoff = 20){
-  test = unlist(lapply(as.vector(titles), strsplit, split = ' ', fixed = TRUE))
+  test = unlist(lapply(as.vector(titles), strsplit, split = ' ', fixed = FALSE))
   stopwords = test %>%
     table() %>%
     sort(decreasing = TRUE) %>%
@@ -27,7 +43,7 @@ StopWordMaker = function(titles, cutoff = 20){
 ## 2) Auto-creating word matrix
 
 
-Embedding_Matrix = function(sent, vocab_min, stopwords, skip_gram, vector_size){
+Embedding_Matrix = function(sent, vocab_min, stopwords, skip_gram, vector_size, iterations = 20){
   it   = itoken(unlist(sent), tolower, word_tokenizer)
   vocab = create_vocabulary(it, stopwords = stopwords)
   vocab <- prune_vocabulary(vocab, term_count_min = vocab_min)
@@ -36,10 +52,9 @@ Embedding_Matrix = function(sent, vocab_min, stopwords, skip_gram, vector_size){
   # use window of 5 for context words
   tcm <- create_tcm(it, vectorizer, skip_grams_window = skip_gram)
   glove = GlobalVectors$new(word_vectors_size = vector_size, vocabulary = vocab, x_max = 5)
-  glove$fit_transform(tcm, n_iter = 20)
+  glove$fit_transform(tcm, n_iter = iterations)
   return(glove$components)
 }
-colnames(temp)
 
 ## 3) Sentence Converter
 
@@ -55,54 +70,52 @@ Vector_puller = function(word, emb_matrix){
 
 Sentence_Vector = function(sentence, emb_matrix, stopwords){
   words = strsplit(sentence, " ", fixed = TRUE)[[1]]
-  #  for(i in 1:4){
-  #    Vector_puller(word[i], emb_matrix)
-  #
-  #  }
   vec = lapply(words, Vector_puller, emb_matrix)
-  df <- data.frame(matrix(unlist(vec), nrow=ncol(emb_matrix), byrow=F))
-  return(rowSums(df)/length(unique(colSums(df))))
+  df = matrix(unlist(vec), ncol = 50, byrow = T)
+  zeros = which(rowSums(df) == 0)
+  if(length(zeros) == 0)
+  {
+    df = apply(df, 2, mean)
+    return(df)
+  }
+  else if(length(zeros) == nrow(df)){
+    return(rep(0, 50))
+  }
+  else if(length(zeros) == nrow(df)-1){
+    return(df[-zeros,])
+  }
+  else{
+    df = apply(df[-zeros,], 2, mean)
+    return(df)
+  }
 }
 
 
 
 # Testing
 library(e1071)
-test_titles = unlist(Pretreatment(dat$Test$Title))
+library(LilRhino)
+library(forcats)
+subs = c("physics", "bio", "med", "geo", "chem", "astro", "eng")
+askscience_Data$tag = askscience_Data$tag %>%
+  fct_collapse("Other" = c(as.character(unique(askscience_Data$tag)[which(!(unique(askscience_Data$tag) %in% subs))])))
 
+askscience_Data$Title = unlist(Pretreatment(askscience_Data$Title))
+#These suck I need better data
+emb_mat = Embedding_Matrix(askscience_Data$Title, 5L, stopwords, 3L, 50, 100)
+
+dat = Cross_val_maker(askscience_Data, .2)
 test_vecs = dat$Test$Title %>%
-  Pretreatment() %>%
-  lapply(., Sentence_Vector, word_vecs, stopwords) %>%
+  lapply(., Sentence_Vector, t(emb_mat), stopwords) %>%
   unlist %>%
-  matrix(nrow = length(dat$Test$Title), byrow=T) %>%
+  matrix(ncol = 50, byrow=T) %>%
   cbind(dat$Test$tag) %>%
   data.frame()
 
 train_vecs = dat$Train$Title %>%
   Pretreatment() %>%
   unlist() %>%
-  lapply(., Sentence_Vector, word_vecs, stopwords) %>%
+  lapply(., Sentence_Vector, t(emb_mat), stopwords) %>%
   unlist() %>%
-  matrix(nrow = length(dat$Train$Title), byrow=T) %>%
-  cbind(dat$Train$tag) %>%
-  data.frame() 
+  matrix(ncol = 50, byrow=T)
 
-fitSVM = svm(X51~., data = train_vecs, type = 'C-classification', kernel = )
-sum(predict(fitSVM, test_vecs[,-51]) == test_vecs$X51)/898
-
-sum(predict(fitSVM, train_vecs[,-51]) == train_vecs$X51)/5089
-
-
-
-FeedData = Feed_Reduction(as.matrix(train_vecs[,-51]), train_vecs$X51, as.matrix(test_vecs[,-51]))
-
-
-
-FeedTrain = FeedData$train
-FeedTest = FeedData$test
-
-annoying = cbind(rbind(FeedTrain, FeedTest), c(train_vecs$X51, test_vecs$X51))
-annoying = as.data.frame(annoying)
-Feedmod = svm(V9~., data = annoying, subset= c(1:5089), type = 'C-classification')
-sum(predict(Feedmod, annoying[c(1:5089),]) == train_vecs$X51)/length(train_vecs$X51)
-sum(predict(Feedmod, annoying[-c(1:5089),]) == test_vecs$X51)/length(test_vecs$X51)
