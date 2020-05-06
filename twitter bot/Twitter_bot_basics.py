21c21debed52f212fc0962194c777f57443f02e9
Travis j Barton <39603097+Travis-Barton@users.noreply.github.com>
2020-05-06 15:39:01 -0700

    Update Twitter_bot_basics.py

--- twitter bot/Twitter_bot_basics.py
+++ twitter bot/Twitter_bot_basics.py
@@ -8,13 +8,13 @@ import tweepy
 import tkinter
 consumer_key = 'QhV5mrXMuX9z54Z2tYdHC1I3U'
 consumer_secret = '2ouNVyhrkcy4LQoWTVi2iuTdcPAphPnsjmJAcXGx98P0oVp7oM'
-access_token = '107270162-toI6Q1a3z85xf01QfzmCSyHxkMx3z96PbtGyL78F'
-access_token_secret = '8n9onMf9c7k7yFEM42qfzfvTAKwAJ9tEgJp0p6Ldi4Dlu'
+access_token = '107270162-XhOEqjjXAnu2hDQbuK7NaGDDnvE2CAUk23duciwN'
+access_token_secret = 'fK3BxLVCYbhzeIGyy52nS6B0nGQHNzd0u1pVPWDJ6cKvC'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-user = api.me()
-print (user.name)
-api.update_status(status = 'Trying to update my status with python!')
-api.update_status(status = 'Ayyyye it worked, now to build bots')
+# user = api.me()
+# print (user.name)
+# api.update_status(status = 'Trying to update my status with python!')
+# api.update_status(status = 'Ayyyye it worked, now to build bots')
