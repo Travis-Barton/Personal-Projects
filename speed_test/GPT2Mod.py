a3d5d9dda8256ef55327799bf20c6d2481b1bff2
Travis j Barton <39603097+Travis-Barton@users.noreply.github.com>
2021-06-28 16:41:21 -0700

    pyupdate

--- speed_test/GPT2Mod.py
+++ speed_test/GPT2Mod.py
@@ -16,23 +16,26 @@ if tf.__version__ == '2.0.0':
     subprocess.check_call([sys.executable, 'conda', 'install', 'tensorflow==1.15.0'])
 import gpt_2_simple as gpt2
 
-# gpt2.download_gpt2(model_name='774m')
+gpt2.download_gpt2(model_name='774m')
 # gpt2.download_gpt2()
 
 
 sess = gpt2.start_tf_sess()
+#sess = gpt2.load_gpt2(sess, 'second_run', multi_gpu=True)
 gpt2.finetune(sess,
-              dataset='chessbot.txt',
-              run_name='second_run',
+              dataset='big_chess_set.txt',
+              run_name='new_run_large',
               print_every=1,
               multi_gpu=True,
               save_every=100,
               combine=100,
-              steps=1000,
-              temerature=.4)   # steps is max number of training steps
+              steps=10000)   # steps is max number of training steps
+
+# print('readying to generate!')
+# single_text = gpt2.generate(sess, prefix='e4, e5 ', return_as_list=True, run_name='large_run')[0]
 
 print('readying to generate!')
-single_text = gpt2.generate(sess, prefix='e4, e5 ', return_as_list=True)[0]
+single_text = gpt2.generate(sess, prefix='e4, e5 ', return_as_list=True, run_name='second_run')[0]
 sess.close()
 print(single_text)      
 
